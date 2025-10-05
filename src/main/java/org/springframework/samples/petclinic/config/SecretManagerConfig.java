e6a42e5f8506e12f54548ef3e83939735fde4231
Gaurav Mohan <gauravmohan85@gmail.com>
2025-10-05 17:03:44 +0200

    pom xml

---
+++ src/main/java/org/springframework/samples/petclinic/config/SecretManagerConfig.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("cloudrun")
+public class SecretManagerConfig {
+    // Spring Cloud GCP Secret Manager auto-configuration does most of the work
+    // This class enables us to conditionally apply this configuration only in cloudrun profile
+}
