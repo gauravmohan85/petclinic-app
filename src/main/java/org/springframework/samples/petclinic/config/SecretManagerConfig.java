117c087171770b499c8b6d841a4864844f4d50c4
Gaurav Mohan <gauravmohan85@gmail.com>
2025-10-05 18:09:17 +0200

    reverted pom

--- src/main/java/org/springframework/samples/petclinic/config/SecretManagerConfig.java
+++ src/main/java/org/springframework/samples/petclinic/config/SecretManagerConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("cloudrun")
 public class SecretManagerConfig {
-    // Spring Cloud GCP Secret Manager auto-configuration does most of the work
-    // This class enables us to conditionally apply this configuration only in cloudrun profile
+
+	// Spring Cloud GCP Secret Manager auto-configuration does most of the work
+	// This class enables us to conditionally apply this configuration only in cloudrun
+	// profile
+
 }
